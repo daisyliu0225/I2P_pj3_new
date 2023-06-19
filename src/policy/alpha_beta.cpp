d8f5378a600ac9196f5b2573c7ffd97b7fc2d6f5
daisyliu0225 <daisyliu0225@gmail.com>
2023-06-19 23:26:09 +0800

    bug_fix
    Since it only creates draws, I try to deal with the problem by adding the maximizing factor.

--- src/policy/alpha_beta.cpp
+++ src/policy/alpha_beta.cpp
@@ -25,7 +25,7 @@ Move Alpha_Beta::get_move(State *state, int depth){
   int mininf = std::numeric_limits<int>::min();
   int maxinf = std::numeric_limits<int>::max();
   for(int i=0;i<sz;i++){
-    int value = getalphabeta(state->next_state(state->legal_actions[i]), depth-1, mininf, maxinf);
+    int value = getalphabeta(state->next_state(state->legal_actions[i]), depth-1, mininf, maxinf, true);
     if(value > bestvalue){
       bestmove = i;
       bestvalue = value;
@@ -34,28 +34,28 @@ Move Alpha_Beta::get_move(State *state, int depth){
   return state->legal_actions[bestmove];
 }
 
-int Alpha_Beta::getalphabeta(State *state, int depth, int alpha, int beta){
+int Alpha_Beta::getalphabeta(State *state, int depth, int alpha, int beta, bool maximizeplayer){
   if(depth == 0 || state->legal_actions.size()==0){
     return state->evaluate();
   }
   // if the tree goes to the final node or reaches the lowest depth, then it gives back the value
 
   int sz = state->legal_actions.size();
-  if(state->player){
+  if(maximizeplayer==true){
     //it is time for the player(you) to choose
     int player_best = std::numeric_limits<int>::min();
     for(int i=0;i<sz;i++){
-      int value = getalphabeta(state->next_state(state->legal_actions[i]), depth-1, alpha, beta);
+      int value = getalphabeta(state->next_state(state->legal_actions[i]), depth-1, alpha, beta ,false);
       if(player_best < value) player_best = value;
       if(value >= beta) break;
       if(alpha < value) alpha = value;
     }
     return player_best;
-  }else if(!state->player){
+  }else if(maximizeplayer==false){
     //it is time for your opponent to choose, he or she should choose the min, which can possibly beat you
     int opp_best = std::numeric_limits<int>::max();
     for(int i=0;i<sz;i++){
-      int value = getalphabeta(state->next_state(state->legal_actions[i]), depth-1, alpha, beta);
+      int value = getalphabeta(state->next_state(state->legal_actions[i]), depth-1, alpha, beta, true);
       if(opp_best > value) opp_best = value;
       if(value <= alpha) break;
       if(beta > value) beta = value;
--- src/policy/minimax.cpp
+++ src/policy/minimax.cpp
@@ -23,7 +23,7 @@ Move Minimax::get_move(State *state, int depth){
   int bestmove = 0;
   int bestvalue = std::numeric_limits<int>::min();
   for(int i=0;i<sz;i++){
-    int value = getminimax(state->next_state(state->legal_actions[i]), depth-1);
+    int value = getminimax(state->next_state(state->legal_actions[i]), depth-1, true);
     if(value > bestvalue){
       bestmove = i;
       bestvalue = value;
@@ -32,26 +32,26 @@ Move Minimax::get_move(State *state, int depth){
   return state->legal_actions[bestmove];
 }
 
-int Minimax::getminimax(State *state, int depth){
+int Minimax::getminimax(State *state, int depth, bool maximizeplayer){
   if(depth == 0 || state->legal_actions.size()==0){
     return state->evaluate();
   }
   // if the tree goes to the final node or reaches the lowest depth, then it gives back the value
 
   int sz = state->legal_actions.size();
-  if(state->player){
+  if(maximizeplayer==true){
     //it is time for the player(you) to choose
     int player_best = std::numeric_limits<int>::min();
     for(int i=0;i<sz;i++){
-      int value = getminimax(state->next_state(state->legal_actions[i]), depth-1);
+      int value = getminimax(state->next_state(state->legal_actions[i]), depth-1, false);
       if(player_best < value) player_best = value;
     }
     return player_best;
-  }else if(!state->player){
+  }else if(maximizeplayer==false){
     //it is time for your opponent to choose, he or she should choose the min, which can possibly beat you
     int opp_best = std::numeric_limits<int>::max();
     for(int i=0;i<sz;i++){
-      int value = getminimax(state->next_state(state->legal_actions[i]), depth-1);
+      int value = getminimax(state->next_state(state->legal_actions[i]), depth-1, true);
       if(opp_best > value) opp_best = value;
     }
     return opp_best;
