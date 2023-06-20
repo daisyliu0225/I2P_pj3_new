7a82ff88f2325e37d0213808b1e50425ecd0981f
daisyliu0225 <daisyliu0225@gmail.com>
2023-06-20 21:44:35 +0800

    finalfinalfinal

--- src/policy/minimax.cpp
+++ src/policy/minimax.cpp
@@ -21,7 +21,7 @@ Move Minimax::get_move(State *state, int depth){
   //std::cout<<"crash"<<std::endl;
   int sz = state->legal_actions.size();
   int bestmove = 0;
-  int bestvalue = -2147483647;
+  int bestvalue = std::numeric_limits<int>::min();
   for(int i=0;i<sz;i++){
     int value = getminimax(state->next_state(state->legal_actions[i]), depth, 0);
     if(value > bestvalue){
@@ -41,17 +41,17 @@ int Minimax::getminimax(State *state, int depth, int maximizeplayer){
   int sz = state->legal_actions.size();
   if(maximizeplayer==1){
     //it is time for the player(you) to choose
-    int player_best = -1000000;
-    for(auto it : state->legal_actions){
-      int value = getminimax(state->next_state(it), depth-1, 0);
+    int player_best = std::numeric_limits<int>::min();
+    for(int i=0;i<sz;i++){
+      int value = getminimax(state->next_state(state->legal_actions[i]), depth-1, 0);
       if(player_best < value) player_best = value;
     }
     return player_best;
   }else{
     //it is time for your opponent to choose, he or she should choose the min, which can possibly beat you
-    int opp_best = 1000000;
-    for(auto it : state->legal_actions){
-      int value = getminimax(state->next_state(it), depth-1, 1);
+    int opp_best = std::numeric_limits<int>::max();
+    for(int i=0;i<sz;i++){
+      int value = getminimax(state->next_state(state->legal_actions[i]), depth-1, 1);
       if(opp_best > value) opp_best = value;
     }
     return opp_best;
